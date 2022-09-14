0f0248ae304e58ee450b8fb8bbc49fc3acc29350
Furrrlo <ferloo@protonmail.com>
2022-09-14 12:06:37 +0200

    Use Bootstrap navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,74 +1,54 @@
 import useUser from '../lib/useUser'
-import { useRouter } from 'next/router'
+import {useRouter} from 'next/router'
+import {Navbar, Container, Nav, NavDropdown} from 'react-bootstrap'
+import Link from "next/link"
+import Image from "next/future/image"
 
 export default function Header() {
-    const { user, mutateUser } = useUser()
+    const {user, mutateUser} = useUser()
     const router = useRouter()
 
     return (
-        <header>
-            <nav>
-                <ul>
-                    <li>
-                        pisello
-                    </li>
-                </ul>
-                <ul className="right">
-                    <li>
-                        {user?.email}
-                    </li>
-                    <li>
-                        <a
-                            href="/api/logout.tsx"
-                            onClick={async (e) => {
+        <Navbar bg="dark" variant="dark" expand="lg">
+            <Container>
+                <Navbar.Brand>
+                    <Image
+                        style={{ filter: "invert(1)" }}
+                        alt=""
+                        src="/logo.svg"
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                    />{' '}
+                    Banda
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="navbar-nav">
+                    <Nav className="me-auto">
+                        {user?.admin && (
+                            <>
+                                <Nav.Link><Link href="/admin/">Lezioni</Link></Nav.Link>
+                                <Nav.Link><Link href="/admin/docenti">Docenti</Link></Nav.Link>
+                                <Nav.Link><Link href="/admin/alunni">Alunni</Link></Nav.Link>
+                            </>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
+                <Navbar.Collapse className="justify-content-end">
+                    <Nav>
+                        <NavDropdown title={user?.email} id="basic-nav-dropdown">
+                            <NavDropdown.Item onClick={async (e) => {
                                 e.preventDefault()
-                                const user = await fetch('/api/logout', { method: 'POST' }).then(res => res.json());
+                                const user = await fetch('/api/logout', {method: 'POST'}).then(res => res.json());
                                 await Promise.all([
                                     mutateUser(user, false),
                                     router.push('/login'),
                                 ])
-                            }}
-                        >
-                            Logout
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-            <style jsx>{`
-              nav {
-                display: flex;
-              }
-              ul {
-                flex: 1 1;
-                display: flex;
-                list-style: none;
-              }
-              ul.right {
-                margin-left: 0;
-                padding-left: 0;
-              }
-              ul.right > li {
-                margin-right: 1rem;
-                display: flex;
-              }
-              ul.right > li:first-child {
-                margin-left: auto;
-              }
-              a {
-                color: #fff;
-                text-decoration: none;
-                display: flex;
-                align-items: center;
-              }
-              a img {
-                margin-right: 1em;
-              }
-              header {
-                padding: 0.2rem;
-                color: #fff;
-                background-color: #333;
-              }
-            `}</style>
-        </header>
+                            }}>Logout</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
---
+++ pages/admin/alunni.tsx
@@ -0,0 +1,23 @@
+import requireAuth from "../../lib/auth";
+import {NextPage} from "next";
+import Layout from "../../components/Layout";
+import {Container} from "react-bootstrap";
+import styles from "../../styles/Home.module.css";
+
+export const getServerSideProps = requireAuth(async (ctx) => {
+    return { props: {} }
+}, true)
+
+const Home: NextPage = (props) => {
+    return <>
+        <Layout requiresAuth>
+            <Container fluid className={styles.container}>
+                <main className={styles.main}>
+                    <div>TODO</div>
+                </main>
+            </Container>
+        </Layout>
+    </>;
+}
+
+export default Home
