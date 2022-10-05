5420e1cf5aec65a8f45fca021a279d7ed9152e77
Furrrlo <ferloo@protonmail.com>
2022-10-05 10:54:21 +0200

    Add api auth middleware

---
+++ middleware.ts
@@ -0,0 +1,28 @@
+import {NextResponse} from "next/server";
+import type {NextRequest} from "next/server";
+import {getIronSession} from "iron-session/edge";
+import {sessionOptions} from "@/lib/session";
+
+export const middleware = async (req: NextRequest) => {
+  const res = NextResponse.next();
+  const session = await getIronSession(req, res, sessionOptions);
+  const {user} = session;
+
+  const isLoginRoute = req.nextUrl.pathname === '/api/login' ||
+    req.nextUrl.pathname === '/api/logout';
+
+  if (!isLoginRoute && user?.isLoggedIn !== true) {
+    return new NextResponse(null, {status: 403});
+  }
+
+  const isAdminRoute = req.nextUrl.pathname.startsWith('/api/admin/');
+  if (isAdminRoute && user?.admin !== true) {
+    return new NextResponse(null, {status: 403});
+  }
+
+  return res;
+};
+
+export const config = {
+  matcher: "/api/:path*",
+};
